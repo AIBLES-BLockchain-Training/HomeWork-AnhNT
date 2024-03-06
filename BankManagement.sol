684763fec2b41c9a68c48f117f99d8680ff208c4
Bertram__904 <119144224+Bertram904@users.noreply.github.com>
2024-03-06 17:00:16 +0700

    Update BankManagement.sol

--- BankManagement.sol
+++ BankManagement.sol
@@ -66,52 +66,18 @@ contract MoneyManagement is BaseUserManagement {
    }
 }
 
-contract LoanManagement is Moneymanagement {
-    address public admin;
-    mapping(address => bool) public users;
-    uint256 public interestRate;
-    
-    event Borrow(address indexed user, uint256 amount, uint256 months, uint256 interest, uint256 totalPayment);
-    event Repay(address indexed user, uint256 amount);
+contract LoanSystem is MoneyManagement {
+    mapping(address => uint256) public loans;
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can perform this operation");
-        _;
+    function borrow(address _borrower, uint256 _amount) public onlyManager {
+        require(balances[_borrower] >= _amount, "Insufficient funds for borrowing");
+        loans[_borrower] += _amount;
+        balances[_borrower] -= _amount;
     }
 
-    constructor(uint256 _interestRate) {
-        admin = msg.sender;
-        interestRate = _interestRate;
-    }
-
-    function registerUser() external {
-        users[msg.sender] = true;
-    }
-
-    function setInterestRate(uint256 _interestRate) external onlyAdmin {
-        require(_interestRate >= 0, "Interest rate must be non-negative");
-        interestRate = _interestRate;
-    }
-
-    function calculateInterest(uint256 _principal, uint256 _months) public view returns (uint256) {
-        uint256 monthlyRate = (_principal * interestRate * _months) / (12 * 100);
-        return monthlyRate;
-    }
-
-    function borrow(uint256 _amount, uint256 _months) external payable {
-        require(users[msg.sender], "Only registered users can borrow");
-        require(_amount > 0, "Borrowed amount must be greater than 0");
-
-        uint256 totalInterest = calculateInterest(_amount, _months);
-        uint256 totalPayment = _amount + totalInterest;
-        
-        require(msg.value >= totalPayment, "Insufficient funds to cover borrowed amount and interest");
-
-        emit Borrow(msg.sender, _amount, _months, totalInterest, totalPayment);
-    }
-
-    function repayLoan() external payable {
-        // Logic to handle repayment
-        emit Repay(msg.sender, msg.value);
+    function repay(address _borrower, uint256 _amount) public onlyManager {
+        require(loans[_borrower] >= _amount, "Cannot repay more than borrowed");
+        loans[_borrower] -= _amount;
+        balances[_borrower] += _amount;
     }
 }
